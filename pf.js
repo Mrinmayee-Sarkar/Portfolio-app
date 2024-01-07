e005000c3c359c9a704301fcbf80d34e83b85861
Mrinmayee Sarkar <mrinmayee.sarkar23@gmail.com>
2024-01-08 00:14:31 +0530

    first commit

---
+++ pf.js
@@ -0,0 +1,29 @@
+let links = document.getElementsByClassName("links")
+console.log(links)
+let content = document.getElementsByClassName("content")
+console.log(content)
+
+
+function openLink(name) {
+    for (var i of links) {
+        i.classList.remove("active-link")
+
+    }
+    for (var i of content) {
+        i.classList.remove("active-content")
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(name).classList.add("active-content")
+}
+
+
+function handleMenu() {
+    var menu = document.getElementById("menu")
+    menu.style.right = "0"
+}
+
+function handleClose() {
+    var menu = document.getElementById("menu")
+    menu.style.right = "-200px"
+}
+
