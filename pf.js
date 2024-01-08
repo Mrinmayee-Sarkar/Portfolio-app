a4e0d17c27f00aacab43770ef374f72d044505a1
Mrinmayee Sarkar <mrinmayee.sarkar23@gmail.com>
2024-01-08 07:01:05 +0530

    Added an alert box for the form

--- pf.js
+++ pf.js
@@ -27,3 +27,19 @@ function handleClose() {
     menu.style.right = "-200px"
 }
 
+
+window.addEventListener("load", function () {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const data = new FormData(form);
+        const action = e.target.action;
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        })
+            .then(() => {
+                alert("Success!");
+            })
+    });
+});
