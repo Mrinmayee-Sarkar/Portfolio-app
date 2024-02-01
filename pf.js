04ecede96beb9e03bdc3a3e36866c772055f0061
Mrinmayee Sarkar <mrinmayee.sarkar23@gmail.com>
2024-02-01 13:56:19 +0530

    Added a skill section

--- pf.js
+++ pf.js
@@ -1,28 +1,11 @@
-let links = document.getElementsByClassName("links")
-let content = document.getElementsByClassName("content")
-
-
-function openLink(name) {
-    for (var i of links) {
-        i.classList.remove("active-link")
-
-    }
-    for (var i of content) {
-        i.classList.remove("active-content")
-    }
-    event.currentTarget.classList.add("active-link")
-    document.getElementById(name).classList.add("active-content")
-}
-
-
 function handleMenu() {
-    var menu = document.getElementById("menu")
+    let menu = document.getElementById("menu")
     menu.style.right = "0"
 
 }
 
 function handleClose() {
-    var menu = document.getElementById("menu")
+    let menu = document.getElementById("menu")
     menu.style.right = "-65vw"
 }
 
@@ -30,9 +13,9 @@ function handleClose() {
 
 document.addEventListener("DOMContentLoaded",function (){
     
-var modal1 = document.getElementById("myModal1")
-var btn1 = document.getElementById("rdmore1")
-var span1 = document.getElementsByClassName("close1")[0]
+let modal1 = document.getElementById("myModal1")
+let btn1 = document.getElementById("rdmore1")
+let span1 = document.getElementsByClassName("close1")[0]
 
 btn1.onclick = function() {
   modal1.style.display = "block"
@@ -43,9 +26,9 @@ span1.onclick = function() {
 })
 
 document.addEventListener("DOMContentLoaded",function (){
-var modal2 = document.getElementById("myModal2")
-var btn2 = document.getElementById("rdmore2")
-var span2 = document.getElementsByClassName("close2")[0]
+let modal2 = document.getElementById("myModal2")
+let btn2 = document.getElementById("rdmore2")
+let span2 = document.getElementsByClassName("close2")[0]
 
 btn2.onclick = function() {
   modal2.style.display = "block"
@@ -55,9 +38,9 @@ span2.onclick = function() {
 }
 })
 document.addEventListener("DOMContentLoaded",function (){
-var modal3 = document.getElementById("myModal3")
-var btn3 = document.getElementById("rdmore3")
-var span3 = document.getElementsByClassName("close3")[0]
+let modal3 = document.getElementById("myModal3")
+let btn3 = document.getElementById("rdmore3")
+let span3 = document.getElementsByClassName("close3")[0]
 
 btn3.onclick = function() {
   modal3.style.display = "block"
@@ -68,9 +51,9 @@ span3.onclick = function() {
 
 })
 document.addEventListener("DOMContentLoaded",function (){
-var modal4 = document.getElementById("myModal4")
-var btn4 = document.getElementById("rdmore4")
-var span4 = document.getElementsByClassName("close4")[0]
+let modal4 = document.getElementById("myModal4")
+let btn4 = document.getElementById("rdmore4")
+let span4 = document.getElementsByClassName("close4")[0]
 
 btn4.onclick = function() {
   modal4.style.display = "block"
